a67d8f5cbcc50a7ad0be47a029294a075ca4b57e
tyler36 <7234392+tyler36@users.noreply.github.com>
2025-03-11 17:57:51 +0900

    add 2-way binding

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,13 +3,21 @@
     <div class="count text-center text-5xl my-8">{{ storeCounter.count }}</div>
 
     <div class="flex gap-x-2 justify-center mt-6">
-      <button @click="storeCounter.decreaseCount" class="px-2 py-1 border rounded border-gray-400 hover:bg-gray-300">-</button>
-      <button @click="storeCounter.increaseCount" class="px-2 py-1 border rounded border-gray-400 hover:bg-gray-300">+</button>
+      <button @click="storeCounter.decreaseCount"
+        class="px-2 py-1 border rounded border-gray-400 hover:bg-gray-300">-</button>
+      <button @click="storeCounter.increaseCount"
+        class="px-2 py-1 border rounded border-gray-400 hover:bg-gray-300">+</button>
     </div>
 
     <hr>
 
     <div>The counter is: {{ storeCounter.oddOrEven }}</div>
+    <hr>
+
+    <div class="flex gap-x-2">
+      <h3>Edit counter:</h3>
+      <input type="number" v-model="storeCounter.count" class="border rounded border-gray-500 px-2">
+    </div>
   </div>
 </template>
 
