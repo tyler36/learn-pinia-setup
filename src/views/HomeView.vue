468d3dfc62925cfb84b317e09f2f3db099a9808d
tyler36 <7234392+tyler36@users.noreply.github.com>
2025-03-11 16:44:30 +0900

    scaffold counter

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="home">
-    <h1>Home</h1>
+  <div class="home text-center">
+    <div class="count text-center text-5xl my-8">0</div>
+
+    <div class="flex gap-x-2 justify-center mt-6">
+      <button class="px-2 py-1 border rounded border-gray-400 hover:bg-gray-300">-</button>
+      <button class="px-2 py-1 border rounded border-gray-400 hover:bg-gray-300">+</button>
+    </div>
   </div>
 </template>
