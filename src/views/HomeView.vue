d0bca6ded2644abbbb98871c740b2b25bd7ee0a6
tyler36 <7234392+tyler36@users.noreply.github.com>
2025-03-11 16:50:25 +0900

    add click handlers for buttons

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,22 @@
 <template>
   <div class="home text-center">
-    <div class="count text-center text-5xl my-8">0</div>
+    <div class="count text-center text-5xl my-8">{{ count }}</div>
 
     <div class="flex gap-x-2 justify-center mt-6">
-      <button class="px-2 py-1 border rounded border-gray-400 hover:bg-gray-300">-</button>
-      <button class="px-2 py-1 border rounded border-gray-400 hover:bg-gray-300">+</button>
+      <button @click="decreaseCount" class="px-2 py-1 border rounded border-gray-400 hover:bg-gray-300">-</button>
+      <button @click="increaseCount" class="px-2 py-1 border rounded border-gray-400 hover:bg-gray-300">+</button>
     </div>
   </div>
 </template>
+
+<script setup>
+import { ref } from "vue";
+const count = ref(5)
+
+function increaseCount() {
+  count.value++
+}
+function decreaseCount() {
+  count.value--
+}
+</script>
