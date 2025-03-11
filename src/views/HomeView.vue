56e6ee9a9e8d7ff4344bcdc23bd818b3336a0319
tyler36 <7234392+tyler36@users.noreply.github.com>
2025-03-11 17:06:35 +0900

    setup pinia store

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home text-center space-y-4">
-    <div class="count text-center text-5xl my-8">{{ count }}</div>
+    <div class="count text-center text-5xl my-8">{{ storeCounter.count }}</div>
 
     <div class="flex gap-x-2 justify-center mt-6">
       <button @click="decreaseCount" class="px-2 py-1 border rounded border-gray-400 hover:bg-gray-300">-</button>
@@ -15,7 +15,10 @@
 
 <script setup>
 import { ref, computed } from "vue";
-const count = ref(5)
+import { useCounterStore } from "@/stores/counter";
+
+const storeCounter = useCounterStore();
+const count = ref(0)
 
 function increaseCount() {
   count.value++
