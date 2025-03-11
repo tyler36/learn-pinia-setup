4e976f19c3567426e40eeeeb9a9ca53c447ecbf5
tyler36 <7234392+tyler36@users.noreply.github.com>
2025-03-11 18:00:32 +0900

    use store everywhere

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
     <h1>About</h1>
+    <button @click="storeCounter.increaseCount" class="px-2 py-1 border rounded border-gray-400 hover:bg-gray-300">{{
+      storeCounter.count }}</button>
   </div>
 </template>
+
+<script setup>
+import { useCounterStore } from "@/stores/counter";
+
+const storeCounter = useCounterStore();
+</script>
