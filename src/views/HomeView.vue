20c0dd2c670e05844db079af33031e6e5d4f5fba
tyler36 <7234392+tyler36@users.noreply.github.com>
2025-03-11 16:52:57 +0900

    computer even/odd state

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class="home text-center">
+  <div class="home text-center space-y-4">
     <div class="count text-center text-5xl my-8">{{ count }}</div>
 
     <div class="flex gap-x-2 justify-center mt-6">
       <button @click="decreaseCount" class="px-2 py-1 border rounded border-gray-400 hover:bg-gray-300">-</button>
       <button @click="increaseCount" class="px-2 py-1 border rounded border-gray-400 hover:bg-gray-300">+</button>
     </div>
+
+    <hr>
+
+    <div>The counter is: {{ oddOrEven }}</div>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 const count = ref(5)
 
 function increaseCount() {
@@ -19,4 +23,10 @@ function increaseCount() {
 function decreaseCount() {
   count.value--
 }
+
+const oddOrEven = computed(() => {
+  if (count.value % 2 === 0 ) return 'even'
+
+  return 'odd'
+})
 </script>
